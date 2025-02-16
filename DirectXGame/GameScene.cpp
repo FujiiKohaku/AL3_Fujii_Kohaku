3e772f28b42da94c652c13300f9df2aae70769d2
FujiiKohaku <k024g0078@g.neec.ac.jp>
2025-02-16 21:18:57 +0900

    最新

---
+++ DirectXGame/GameScene.cpp
@@ -0,0 +1,8 @@
+#include "GameScene.h"
+
+GameScene::GameScene() {}
+
+GameScene::~GameScene() {}
+void GameScene::Initialize() {}
+void GameScene::Update() {}
+void GameScene::Draw() {}
---
+++ DirectXGame/GameScene.h
@@ -0,0 +1,9 @@
+#pragma once
+class GameScene {
+public:
+	GameScene();
+	~GameScene();
+	void Initialize() {}
+	void Update() {}
+	void Draw() {}
+	};
